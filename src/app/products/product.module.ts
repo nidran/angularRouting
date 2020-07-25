37287416bd0051924c7426c091be8680451d5348
nidran <nidhi.ranjan145@gmail.com>
2020-07-25 14:14:00 +0530

    adding resolver to route controller

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -3,16 +3,20 @@ import {RouterModule} from '@angular/router';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
-
+import { ProductResolver } from './product-resolver.service';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path:'products', component: ProductListComponent},
-      { path:'products/:id', component: ProductDetailComponent},
-      { path:'products/:id/edit', component: ProductEditComponent}
+      {  path:'products', component: ProductListComponent},
+      { path:'products/:id',
+       component: ProductDetailComponent , 
+      resolve: {resolvedData : ProductResolver}},
+      { path:'products/:id/edit', 
+      component: ProductEditComponent , 
+      resolve : {resolvedData : ProductResolver}}
     ])
   ],
   declarations: [
