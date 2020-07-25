46af51ad01622bcf7e7aa261a254ddb5e9a7e3d4
nidran <nidhi.ranjan145@gmail.com>
2020-07-25 14:43:25 +0530

    configureing child routers

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -3,6 +3,8 @@ import {RouterModule} from '@angular/router';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import {ProductEditInfoComponent} from './product-edit/product-edit-info.component';
+import {ProductEditTagsComponent} from './product-edit/product-edit-tags.component';
 import { ProductResolver } from './product-resolver.service';
 import { SharedModule } from '../shared/shared.module';
 
@@ -16,13 +18,30 @@ import { SharedModule } from '../shared/shared.module';
       resolve: {resolvedData : ProductResolver}},
       { path:'products/:id/edit', 
       component: ProductEditComponent , 
-      resolve : {resolvedData : ProductResolver}}
+      resolve : {resolvedData : ProductResolver},   
+      children :[
+        {
+          path:'',
+          redirectTo: 'info',
+          pathMatch:'full'
+        },
+        {
+          path:'info',
+          component: ProductEditInfoComponent
+        },
+        {
+          path:'',
+          component: ProductEditTagsComponent 
+        }
+      ]}
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
